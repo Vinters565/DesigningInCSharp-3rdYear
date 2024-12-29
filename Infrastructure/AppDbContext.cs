5cbdcfd9e85b7a2eee28fd630ffd4c51b2dbf07c
Vinters565 <mrvinters565@mail.ru>
2024-12-29 17:11:47 +0500

    MiniFix

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -11,7 +11,7 @@ namespace SchedulePlanner.Infrastructure
         public DbSet<CalendarEvent> CalendarEvents { get; set; }
         public DbSet<User> Users { get; set; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) => Database.EnsureCreated();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
