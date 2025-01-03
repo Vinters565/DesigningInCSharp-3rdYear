2593ca1a9fb8ff7a8ae3cf4cbeb1bbc7a422a24d
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2025-01-03 21:06:35 +0500

    add migration "addUserMigration"

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -5,7 +5,7 @@ namespace SchedulePlanner.Domain.Entities;
 public class User(Guid id, string username, string displayedName, string passwordHash)
     : Entity<Guid>(id)
 {
-    public string Username { get; } = username;
-    public string DisplayedName { get; } = displayedName;
-    public string PasswordHash { get; } = passwordHash;
+    public string Username { get; init; } = username;
+    public string DisplayedName { get; init; } = displayedName;
+    public string PasswordHash { get; init; } = passwordHash;
 }
