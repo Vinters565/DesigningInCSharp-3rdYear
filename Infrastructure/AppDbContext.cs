bae8922545afddccd8a288c7c3655acdd6410c24
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2025-01-04 05:48:12 +0500

    add migration "addSubscriptionsMigration"

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -11,6 +11,8 @@ namespace SchedulePlanner.Infrastructure
         public DbSet<CalendarEvent> CalendarEvents { get; set; }
         public DbSet<User> Users { get; set; }
 
+        public DbSet<Subscription> Subscriptions { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) => Database.EnsureCreated();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ Infrastructure/Migrations/20250104004740_addSubscriptionsMigration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchedulePlanner.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSubscriptionsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CalendarEventId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subscriptions_CalendarEvents_CalendarEventId",
+                        column: x => x.CalendarEventId,
+                        principalTable: "CalendarEvents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Subscriptions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_CalendarEventId",
+                table: "Subscriptions",
+                column: "CalendarEventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_UserId",
+                table: "Subscriptions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscriptions");
+        }
+    }
+}
