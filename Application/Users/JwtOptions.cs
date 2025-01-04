09802f1694eebbe349fc2a12668550b7efd5c1fd
Kiec00 <user.nullptr@gmail.com>
2025-01-04 21:23:26 +0500

    Move jwt secret key to appsettings

--- Application/Users/JwtOptions.cs
+++ Application/Users/JwtOptions.cs
@@ -3,5 +3,5 @@ namespace SchedulePlanner.Application.Users;
 public class JwtOptions
 {
     public TimeSpan Expires { get; set; }
-    public string? SecretKey { get; set; }
+    public string SecretKey { get; set; }
 }
