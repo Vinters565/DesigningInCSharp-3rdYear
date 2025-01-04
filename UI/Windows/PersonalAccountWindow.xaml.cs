73ac4e21a72dcec444426726fed6c4953e9e7f51
Ekaterina Tarakanova <ktarakanova@yandex.ru>
2025-01-04 17:16:30 +0500

    PersonalAccountWindow

---
+++ UI/Windows/PersonalAccountWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace UI.Windows
+{
+    public partial class PersonalAccountWindow : Window
+    {
+        public PersonalAccountWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
