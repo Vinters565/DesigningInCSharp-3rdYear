0b03310c38af06c833b586ccc941f96df89c4866
Ekaterina Tarakanova <146315283+EkaterinaTarakanova@users.noreply.github.com>
2025-01-12 18:10:37 +0500

    EventInfoPage

---
+++ UI/ElementPage/EventInfoPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using UI.Windows;
+
+namespace UI.ElementPage
+{
+    public partial class EventInfoPage : Page
+    {
+        public EventInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EditEventButton_Click(object sender, RoutedEventArgs e)
+        {
+            var editEventPage = new EditEventPage();
+            NavigationService.Navigate(editEventPage);
+        }
+
+        private void DeleteEventButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
